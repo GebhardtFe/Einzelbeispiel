6f38ce75d8f546864dd4c0bbbefd5d66abec331a
GebhardtFe <56358595+GebhardtFe@users.noreply.github.com>
2021-03-13 09:30:30 +0100

    ClientSocket Funktioniert

--- app/src/main/java/gebhardtfe/seii/einzelbeispiel/Einzelbeispiel.java
+++ app/src/main/java/gebhardtfe/seii/einzelbeispiel/Einzelbeispiel.java
@@ -3,18 +3,27 @@ package gebhardtfe.seii.einzelbeispiel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.NumberPicker;
 
 public class Einzelbeispiel extends AppCompatActivity {
 
     private TextView tHeadline ;
     private TextView tDiscription;
+    private TextView tAnswer;
 
     private Button bSend;
     private Button bCalculate;
+
+    private EditText iNumber;
+
+    private Handler response = new Handler();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,9 @@ public class Einzelbeispiel extends AppCompatActivity {
 
         tHeadline = findViewById(R.id.tHeadline);
         tDiscription = findViewById(R.id.tDiscription);
+        tAnswer = findViewById(R.id.tAnswer);
+
+        iNumber = findViewById(R.id.iNumber);
 
         bSend = findViewById(R.id.bSend);
         bCalculate = findViewById(R.id.bCalculate);
@@ -32,7 +44,8 @@ public class Einzelbeispiel extends AppCompatActivity {
         bSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("MyApp", "FirstClickDone");
+                tcpClient client = new tcpClient(iNumber.getText().toString(), response);
+                new Thread(client).start();
             }
         });
 
@@ -43,8 +56,9 @@ public class Einzelbeispiel extends AppCompatActivity {
             }
         });
 
+        iNumber.setText("11926537");
 
         Log.d("Test","123");
-
     }
+
 }
---
+++ app/src/main/java/gebhardtfe/seii/einzelbeispiel/tcpClient.java
@@ -0,0 +1,49 @@
+package gebhardtfe.seii.einzelbeispiel;
+
+import android.os.Handler;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.*;
+import java.net.*;
+
+
+public class tcpClient implements Runnable{
+    private String input;
+    private String answer;
+    private Socket clientSocket;
+
+    Handler response;
+
+
+    public tcpClient(String input, Handler response){
+        this.input = input;
+        this.response = response;
+    }
+
+    @Override
+    public void run() {
+        try {
+            openSocketandSend();
+        }catch (IOException e){
+            Log.e("Test","Kein Verbindungsaufbau möglich!");
+        }
+    }
+
+
+    public void openSocketandSend() throws IOException{
+        clientSocket = new Socket("se2-isys.aau.at", 53212);
+        DataOutputStream outToServer =
+                new DataOutputStream(clientSocket.getOutputStream());
+
+        BufferedReader inFromServer =
+                new BufferedReader(new
+                        InputStreamReader(clientSocket.getInputStream()));
+
+        outToServer.writeBytes(input+"\n");
+        answer = inFromServer.readLine();
+        Log.d("Test",answer);
+        clientSocket.close();
+        Log.d("Test",answer);
+    }
+}
